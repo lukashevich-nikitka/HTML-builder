70444a95cb28edd3a97ef87c9b30d487758b1490
Nikita <lukashevich-nikitka@bk.ru>
2022-05-23 18:39:58 +0300

    feat: fix: fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,14 +4,18 @@ fs.mkdir('./04-copy-directory/files-copy', {recursive: true}, (err) => {
   if (err) throw err;
 });
 
+const files = fs.promises.readdir('./04-copy-directory/files', {withFileTypes: true});
+for (let file in files) {
+  fs.rmdir('./04-copy-directory/files-copy' + '/' + file.name, (err) => {
+    if (err) throw err;
+  });
+}
+
 async function copyDir() {
   const files = await fs.promises.readdir('./04-copy-directory/files', {withFileTypes: true});
   for (let file of files) {
-    const writeStream = await fs.createWriteStream('./04-copy-directory/files-copy' + '/' + file.name);
-    fs.truncate('./04-copy-directory/files-copy' + '/' + file.name, (err) => {
-      if (err) throw err;
-    });
-    const readableStream =  fs.createReadStream('./04-copy-directory/files' + '/' + file.name);
+    const writeStream = fs.createWriteStream('./04-copy-directory/files-copy' + '/' + file.name);
+    const readableStream = fs.createReadStream('./04-copy-directory/files' + '/' + file.name);
     readableStream.on('data', function (chunk) {
       writeStream.write(chunk);
     });
